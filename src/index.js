64345365e3a2965d193a76542ad534051105f878
jrmaiafs <67764559+jrmaiafs@users.noreply.github.com>
2021-10-21 11:27:59 -0300

    added route create users and verify if user alteady exists

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const {v4: uuidv4} = require('uuid');
 
 // const { v4: uuidv4 } = require('uuid');
 
@@ -8,14 +9,31 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// const users = [];
+const users = [];
 
 function checksExistsUserAccount(request, response, next) {
   // Complete aqui
 }
 
 app.post('/users', (request, response) => {
-  // Complete aqui
+  const {name, username} = request.body
+
+  const usersExists = users.some((user) => user.username === username);
+
+  if (usersExists) {
+    return response.status(400).json({error: "Esse nome de usuário já existe!"})
+  }
+
+  const user = {
+    id: uuidv4(),
+    name,
+    username,
+    todos: [],
+  }
+
+  users.push(user)
+  
+  return response.status(201).json(user)
 });
 
 app.get('/todos', checksExistsUserAccount, (request, response) => {
